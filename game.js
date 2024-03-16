cc966b00592fe6da2b736ab1715673116c31f077
Zach Myre <myrezach@live.com>
2024-03-16 13:14:45 -0500

    drawball and draw loop (game loop)

--- game.js
+++ game.js
@@ -3,6 +3,35 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+let dx = 2;
+let dy = -2;
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+   
+
+// Game Loop
+function draw(){
+    // circle
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+// Game Loop Interval with FPS
+setInterval(draw, 1000 / 60);
+
+
 // rectangle
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
@@ -10,9 +39,12 @@ ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
-// circle
+
+
+//unfilled rectangle
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "blue";
-ctx.fill();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgb(0 0 255 / 50%)";
+ctx.stroke();
 ctx.closePath();
+
